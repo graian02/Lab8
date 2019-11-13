49750ea5936e9f279495b85be4a3b86bebfd359d
Graian <sohgraian@gmail.com>
2019-11-13 10:16:30 +0800

    version1

---
+++ Ex5.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestMyProgranm(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(),'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('foo'.isupper())
+if __name__== '__main__':
+    unittest.main()
